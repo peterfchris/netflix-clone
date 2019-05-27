479fc2dacf0a1449b9ea0dd3e58c59a6ab743f3e
Peter Christensen <peter.f.chris@gmail.com>
2019-05-27 14:06:44 -0600

    everything I'm willing to do today

--- media-queries-afternoon/src/App.js
+++ media-queries-afternoon/src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div>
-    
+      <div className="top-bit">
+        <nav>
+          <span>
+            <h3>Start Bootstrap</h3>
+          </span>
+          <ul>
+            <li>services</li>
+            <li>portfolio</li>
+            <li>about</li>
+            <li>team</li>
+            <li>contact</li>
+          </ul>
+        </nav>
+        <div className="words">
+          <h2>Welcome To Our Studio!</h2>
+          <h1>it's nice to meet you</h1>
+          <button>tell me more</button>
+        </div>
+      </div>
     </div>
   );
 }
